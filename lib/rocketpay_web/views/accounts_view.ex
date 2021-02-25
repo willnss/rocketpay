7005c17e464faa11128f5530311b4e2242290c5e
willianns <willianns@users.noreply.github.com>
2021-02-25 06:18:11 -0500

    concluindo implementação de deposito

---
+++ lib/rocketpay_web/views/accounts_view.ex
@@ -0,0 +1,12 @@
+defmodule RocketpayWeb.AccountsView do
+  alias Rocketpay.{Account}
+
+  def render("update.json", %{ account: %Account{id: account_id, balance: balance}}) do
+    %{
+      account: %{
+        id: account_id,
+        balance: balance
+      }
+    }
+  end
+end
